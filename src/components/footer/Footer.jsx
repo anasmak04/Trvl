549aa41da0a798491156bab628113921fb140344
anasmak03 <anas11.makhloufi@gmail.com>
2022-07-16 12:10:36 +0100

    not done

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./Footer.css";
+function footer () {
+    
+        return (
+            <div>
+                <h1>footer</h1>
+            </div>
+        );
+    
+}
+
+export default footer;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./Header.css";
+function Header () {
+    
+        return (
+            <div>
+                <h1>header</h1>
+            </div>
+        );
+    
+}
+
+export default Header;
