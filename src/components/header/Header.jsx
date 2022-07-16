d105e3474031fd27ba9af7b966bcf3cc9ca98370
anasmak03 <anas11.makhloufi@gmail.com>
2022-07-16 12:24:01 +0100

    not done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import "./Header.css";
 function Header () {
     
         return (
             <div>
-                <h1>header</h1>
+        <Router>
+          <header>
+                <nav className='navbar'>
+                    <div className="logo">
+                        <h1>logo</h1>
+                    </div>    
+                    <ul className={click ? "nav-menu active" : "nav-menu"}>
+                        <li className='nav-item'><Link to="/" onClick={handleClick}  className='nav-link ' >home</Link></li>
+                        <li className='nav-item'><Link to="/" onClick={handleClick}  className='nav-link' >services</Link></li>
+                        <li className='nav-item'><Link to="/" onClick={handleClick}  className='nav-link' href="">products</Link></li>
+                        <li className='nav-item'><Link to="/" onClick={handleClick}  className='nav-link' href="">sign up</Link></li>
+                    </ul>
+
+                    <div onClick={handleClick} className="nav-icon">
+                    <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+                </div>
+
+                </nav>  
+
+             
+            </header> 
+
+            <Routes>
+                    <Route path='/' element={< home/>}></Route>
+                    <Route path='/services' element={< services/>}></Route>
+                    <Route path='/products' element={< products/>}></Route>
+                    <Route path='/signup' element={< signup/>}></Route>
+                </Routes> 
+          </Router>
             </div>
         );
     
