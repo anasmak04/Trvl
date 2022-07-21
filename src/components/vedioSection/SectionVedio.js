e5763de1214ebf82d6f68d7cc8a207db56ace7e8
anasmak03 <anas11.makhloufi@gmail.com>
2022-07-21 22:42:18 +0100

    header done

--- src/components/vedioSection/SectionVedio.js
+++ src/components/vedioSection/SectionVedio.js
@@ -3,8 +3,16 @@ import "./Section.css"
 function SectionVedio (){
 
         return (
-            <div>
+            <div className='container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <div className='infos'>
+        <h1>adventure awaits</h1>
+        <h2>what are you waiting for ?</h2>
+      </div>
+      <div className='btns'>
+        <button>get started</button>
+        <button>get started</button>
+      </div>
             </div>
         );
     }
